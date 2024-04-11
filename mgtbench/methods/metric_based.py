5087ee5c6efb532b65fe94d15ab4ec76ad39b872
Y-L-LIU <yuki.ucb@gmail.com>
2024-04-11 10:43:42 +0000

    add rankDLTR, fix detect to compatiable for single input

--- mgtbench/methods/metric_based.py
+++ mgtbench/methods/metric_based.py
@@ -6,7 +6,10 @@ from ..utils import timeit, get_clf_results
 from .IntrinsicDim import PHD
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from ..auto import MetricBasedDetector
+from ..auto import BaseDetector
+from ..loading import load_pretrained
+from transformers import PreTrainedModel, PreTrainedTokenizer
+import warnings
 # # Under development
 # def get_phd(text, base_model, base_tokenizer, DEVICE):
 #     # default setting
@@ -30,6 +33,22 @@ from ..auto import MetricBasedDetector
 #     print(time.time() - t1, "Seconds")
 #     return res
 
+class MetricBasedDetector(BaseDetector):
+    def __init__(self, name, **kargs) -> None:
+        super().__init__(name)
+        self.model = kargs.get('model', None)
+        self.tokenizer = kargs.get('tokenizer', None)
+        if not self.model or not not self.tokenizer:
+            model_name_or_path = kargs.get('model_name_or_path', None)
+            if not model_name_or_path :
+                raise ValueError('You should pass the model_name_or_path or a model instance, but none is given')
+            quantitize_bit = kargs.get('load_in_k_bit', None)
+            self.model, self.tokenizer = load_pretrained(model_name_or_path, quantitize_bit)
+        if not isinstance(self.model, PreTrainedModel) or not isinstance(self.tokenizer, PreTrainedTokenizer):
+            raise ValueError('Expect PreTrainedModel, PreTrainedTokenizer, got', type(self.model), type(self.tokenizer))
+        
+
+
 class LLDetector(MetricBasedDetector):
     def __init__(self, name, **kargs) -> None:
         super().__init__(name,**kargs)
@@ -37,6 +56,8 @@ class LLDetector(MetricBasedDetector):
 
     def detect(self, text, **kargs):
         result = []
+        if not isinstance(text, list):
+            text = [text]
         for batch in DataLoader(text):
             with torch.no_grad():
                 tokenized = self.tokenizer(
@@ -46,7 +67,7 @@ class LLDetector(MetricBasedDetector):
                 ).to(self.model.device)
                 labels = tokenized.input_ids
                 result.append( -self.model(**tokenized, labels=labels).loss.item())
-        return result
+        return result if isinstance(text, list) else result[0]
         
  
 class RankDetector(MetricBasedDetector):
@@ -55,6 +76,8 @@ class RankDetector(MetricBasedDetector):
 
     def detect(self, text, **kargs):
         result = []
+        if not isinstance(text, list):
+            text = [text]
         for batch in DataLoader(text):
             with torch.no_grad():
                 tokenized = self.tokenizer(
@@ -82,7 +105,77 @@ class RankDetector(MetricBasedDetector):
             if log:
                 ranks = torch.log(ranks)
             result.append(ranks.float().mean().item())
-        return result       
+        return result if isinstance(text, list) else result[0]       
+
+
+class RankGLTRDetector(MetricBasedDetector):
+    def __init__(self,  **kargs) -> None:
+        super().__init__(**kargs)
+
+    def detect(self, text, **kargs):
+        result = []
+        if not isinstance(text, list):
+            text = [text]
+        for batch in DataLoader(text):
+            with torch.no_grad():
+                tokenized = self.tokenizer(
+                    batch,
+                    max_length=1024,
+                    return_tensors="pt"
+                ).to(self.model.device)
+                logits = self.model(**tokenized).logits[:, :-1]
+                labels = tokenized.input_ids[:, 1:]
+
+        # get rank of each label token in the model's likelihood ordering
+            matches = (logits.argsort(-1, descending=True)
+                    == labels.unsqueeze(-1)).nonzero()
+            assert matches.shape[
+                1] == 3, f"Expected 3 dimensions in matches tensor, got {matches.shape}"
+
+            ranks, timesteps = matches[:, -1], matches[:, -2]
+
+            # make sure we got exactly one match for each timestep in the sequence
+            assert (timesteps == torch.arange(len(timesteps)).to(
+                timesteps.device)).all(), "Expected one match per timestep"
+
+            ranks = ranks.float()
+            res = np.array([0.0, 0.0, 0.0, 0.0])
+            for i in range(len(ranks)):
+                if ranks[i] < 10:
+                    res[0] += 1
+                elif ranks[i] < 100:
+                    res[1] += 1
+                elif ranks[i] < 1000:
+                    res[2] += 1
+                else:
+                    res[3] += 1
+            if res.sum() > 0:
+                res = res / res.sum()
+            result.append(res)
+        return result if isinstance(text, list) else result[0]       
+
+
+class EntropyDetector(MetricBasedDetector):
+    def __init__(self,  **kargs) -> None:
+        super().__init__(**kargs)
+
+    def detect(self, text, **kargs):
+        result = []
+        if not isinstance(text, list):
+            text = [text]
+        for batch in DataLoader(text):
+            with torch.no_grad():
+                tokenized = self.tokenizer(
+                    batch,
+                    truncation=True,
+                    max_length=512,
+                    return_tensors="pt").to(self.model.device)
+                logits = self.model(**tokenized).logits[:, :-1]
+                neg_entropy = F.softmax(logits, dim=-1) * F.log_softmax(logits, dim=-1)
+                result.append( -neg_entropy.sum(-1).mean().item())
+        return result if isinstance(text, list) else result[0]
+
+
 
 def get_ll(text, base_model, base_tokenizer, DEVICE):
     with torch.no_grad():
@@ -177,23 +270,6 @@ def get_rank_GLTR(text, base_model, base_tokenizer, DEVICE, log=False):
 
         return res
 
-class EntropyDetector(MetricBasedDetector):
-    def __init__(self,  **kargs) -> None:
-        super().__init__(**kargs)
-
-    def detect(self, text, **kargs):
-        result = []
-        for batch in DataLoader(text):
-            with torch.no_grad():
-                tokenized = self.tokenizer(
-                    batch,
-                    truncation=True,
-                    max_length=512,
-                    return_tensors="pt").to(self.model.device)
-                logits = self.model(**tokenized).logits[:, :-1]
-                neg_entropy = F.softmax(logits, dim=-1) * F.log_softmax(logits, dim=-1)
-                result.append( -neg_entropy.sum(-1).mean().item())
-        return result
 
 
 # get average entropy of each token in the text
